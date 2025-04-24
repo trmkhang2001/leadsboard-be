43573192d89fd38f7d9cd0a99eaf7fe8e48b5f3e
Minh Khang <trmkhang2001@gmail.com>
2025-04-24 10:29:07 +0700

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "https://leadsboard-fe.vercel.app",
         credentials: true,
     })
 );
